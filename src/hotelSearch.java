b1cd2a8c571a3f73765084f3fd1d8fbc2bbe8643
thordur agust <thorduragust@github.com>
2018-02-24 14:11:45 +0000

    ...

--- src/hotelSearch.java
+++ src/hotelSearch.java
@@ -1,5 +1,46 @@
+//keyrsla: java -cp .:sqlite-jdbc....jar hotelSearch
+import java.sql.*;
+
+class Hotel {
+
+	public String name;
+	public int rating;
+	public String description;
+	public int zipcode;
+
+	public Hotel(String name, int rating, String description, int zipcode) {
+		this.name = name;
+		this.rating = rating;
+		this.description = description;
+		this.zipcode = zipcode;
+	}
+}
+
+
 class hotelSearch {
-	public static void main(String[] args) {
-		System.out.println("hi");
+	public static void main(String[] args) throws ClassNotFoundException {
+		Class.forName("org.sqlite.JDBC");
+		Connection connection = null;
+
+		try {
+			connection = DriverManager.getConnection("jdbc:sqlite:testdb.db");
+			Statement statement = connection.createStatement();
+			ResultSet rs = statement.executeQuery("select name from Hotels");
+
+			while(rs.next()) {
+				System.out.println(rs.getString(1));
+			}
+		}
+		catch(SQLException e) {
+			System.err.println(e.getMessage());
+		}finally {
+			try {
+				if(connection != null) {
+					connection.close();
+				}
+			}catch(SQLException e) {
+				System.err.println(e);
+			}
+		}
 	}
 }
