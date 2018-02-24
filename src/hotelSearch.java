541fd99131f9834986f91f7e131e67b6d1c06645
thordur agust <thorduragust@github.com>
2018-02-24 17:44:03 +0000

    fleiri klasar, en engin föll

--- src/hotelSearch.java
+++ src/hotelSearch.java
@@ -1,5 +1,7 @@
 //keyrsla: java -cp .:sqlite-jdbc....jar hotelSearch
 import java.sql.*;
+import java.util.Date;
+import java.util.ArrayList;
 
 class Hotel {
 
@@ -7,17 +9,72 @@ class Hotel {
 	public int rating;
 	public String description;
 	public int zipcode;
+	public ArrayList<String> tags;
+	public ArrayList<Room> rooms;
 
-	public Hotel(String name, int rating, String description, int zipcode) {
+	public Hotel(String name, int rating, String description, int zipcode, ArrayList<String> tags, ArrayList<Room> rooms) {
 		this.name = name;
 		this.rating = rating;
 		this.description = description;
 		this.zipcode = zipcode;
+		this.tags = tags;
+		this.rooms = rooms;
 	}
+
+	//einhver föll hér
+}
+
+class Room {
+	public int id;
+	public int size;
+	//mögulega eitthvað til að gera þessi eigindi aðeins meira modular... bara mögulega
+	public int bed_count;
+	public boolean shower;
+
+
+	public Room(int id, int size) {
+		this.id = id;
+		this.size = size;
+	}
+
+	//einhver föll hér
+}
+
+class User {
+	public int id;
+	public String name;
+	public String email;
+	public ArrayList<Booking> bookings;
+
+	public User(int id, String name, String email, ArrayList<Booking> bookings) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.bookings = bookings;
+	}
+
+	//einhver föll hér
+}
+
+class Booking {
+	public int id;
+	public Room room;
+	public Date start_date;
+	public Date end_date;
+
+	public Booking(int id, Room room, Date start_date, Date end_date) {
+		this.id = id;
+		this.room = room;
+		this.start_date = start_date;
+		this.end_date = end_date;
+	}
+
+	//einhver föll hér
 }
 
 
 class hotelSearch {
+	//þetta gerir ekkert sérstakt eins og er.
 	public static void main(String[] args) throws ClassNotFoundException {
 		Class.forName("org.sqlite.JDBC");
 		Connection connection = null;
