b6538bb7cdac6afaf99de7e35127d90a6593fc90
Þórður <thordur@htgi-dw0214.rhi.hi.is>
2018-03-21 16:14:26 +0000

    hotelDefinitions skipt i hluta

---
+++ src/User.java
@@ -0,0 +1,24 @@
+import java.util.Date;
+import java.util.ArrayList;
+
+class User {
+	public int id;
+	public String name;
+	public String email;
+	public ArrayList<Booking> bookings;
+
+	public User(int id, String name, String email, ArrayList<Booking> bookings) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.bookings = bookings;
+	}
+
+	public void cancelUnconfirmed() {
+		//...
+	}
+
+	public void cancelBooking(int key) {
+		//...
+	}
+}
