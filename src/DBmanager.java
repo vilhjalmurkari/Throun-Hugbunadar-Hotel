d1cb1f597a84132c9f6826abdefee0bc439a80f9
k <kra33@hi.is>
2018-03-29 12:19:29 +0000

    Yfirfærslu lokið

--- src/DBmanager.java
+++ src/DBmanager.java
@@ -1,16 +1,70 @@
 import java.util.ArrayList;
+import java.sql.*;
 
 public class DBmanager {
 	
-	Connection connection;
-	Statement sqlStatement;
+	static Connection connection;
+	static Statement sqlStatement;
 
 
-///Krisi er ad leika ser
-	public static void init() throws SQLException {
-		Class.forName("org.sqlite.JDBC");
-		connection = DriverManager.getConnection("jdbc:sqlite:testdb.db");
+	// Notkun: init()
+	// Eftir:  Connection og Statement hafa verið upphafsstillt
+	//         svo unnt er að framkvæma SQL aðgerðir.
+	public static void init() {
+		try {
+			Class.forName("org.sqlite.JDBC");
+			connection = DriverManager.getConnection("jdbc:sqlite:testdb.db");
+			sqlStatement = connection.createStatement();
+		} catch( ClassNotFoundException e ) {
+			System.out.println("Couldn't find jdbc file.");
+		} catch( SQLException e ) {
+			System.out.println("Unable to make SQL connection.");
+		}
 	}
+
+	public static ArrayList<Hotel> getAllHotels() throws SQLException {
+		ArrayList<Hotel> listOfHotels = new ArrayList<Hotel>();
+		ResultSet rset = sqlStatement.executeQuery("SELECT * FROM Hotels");
+		while(rset.next()) {
+			ArrayList<Room> roomsInAHotel = getRoomFromHotel(rset.getString("name"), rset.getInt("zipcode"));
+
+			Hotel h = new Hotel(
+				rset.getString("name"),
+				rset.getInt("rating"),
+				rset.getString("description"),
+				rset.getInt("zipcode"),
+				null,
+				roomsInAHotel
+			);
+
+			listOfHotels.add(h);
+		}
+		return listOfHotels;
+	}
+
+	
+	public static ArrayList<Room> getRoomFromHotel( String hotel_name, int hotel_zipcode) throws SQLException {
+		ArrayList<Room> listOfRooms = new ArrayList<Room>();
+		String query = "SELECT * FROM Rooms WHERE hotel_name= \"" + hotel_name +
+			"\" AND hotel_zipcode=" + hotel_zipcode + ";";
+
+		ResultSet rset = sqlStatement.executeQuery(query);
+		
+		while(rset.next()) {
+			Room room = new Room(
+				rset.getInt("id"),
+				rset.getInt("size"),
+				0,
+				false,
+				10000
+			);
+			listOfRooms.add(room);
+		}
+		return listOfRooms;
+	}
+
+
+
 
 	public static void setRoomPrice(double price, ArrayList<Room> rooms) {
 		//...
@@ -29,8 +83,7 @@ public class DBmanager {
 	}
 
 	public static Hotel getHotel(String hotelName, String hotelZip) {
-		//Hotel(String name, int rating, String description, int zipcode, ArrayList<String> tags, ArrayList<Room> rooms)
-		return new Hotel(null, 0, null, 0, null, null);
+		return null;
 	}
 
 	public static void addHotel(Hotel hotel) {
@@ -38,7 +91,7 @@ public class DBmanager {
 		try {
 			Thread.sleep(1000);
 		} catch (InterruptedException e) {
-			System.out.println( "WHAT????");
+			System.out.println( "Something didn't work properly.");
 		}
 	}
 
