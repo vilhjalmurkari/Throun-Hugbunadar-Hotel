2ed7640201ba681447e3b67d0b02fbfa46dbb559
Þórður <thordur@vr2t-dw176.rhi.hi.is>
2018-04-13 13:49:45 +0000

    breytar build skrár og pakka nafnið er annað

--- src/build.bat
+++ src/build.bat
@@ -4,13 +4,12 @@ pushd HotelAPI
 javac Booking.java Hotel.java Room.java User.java SearchQuery.java DBmanager.java HotelAPI.java
 popd
 
-zip -r HotelAPI.zip HotelAPI
+if "%1" == "zip" zip -r HotelAPI.zip HotelAPI
+if "%2" == "zip" zip -r HotelAPI.zip HotelAPI
 
 javac View.java
 
 if %errorlevel% == 0 (
-	if "%1" == "run" (
-		::java -cp .;sqlite-jdbc-3.18.0.jar HotelView
-		java View
-	)
+	if "%1" == "run" java -cp .;sqlite-jdbc-3.18.0.jar HotelView
+	if "%1" == "run" java -cp .;sqlite-jdbc-3.18.0.jar HotelView
 )
--- src/build.sh
+++ src/build.sh
@@ -3,12 +3,14 @@ cd HotelAPI
 javac Booking.java Hotel.java Room.java User.java SearchQuery.java DBmanager.java HotelAPI.java
 cd ..
 
-zip -r HotelAPI.zip HotelAPI
+if [ "$1" == "zip" ] || [ "$2" == "zip" ]; then
+	zip -r HotelAPI.zip HotelAPI
+fi
 
 javac View.java
 
 if [[ "$?" == "0" ]]; then
-	if [[ "$1" == "run" ]]; then
+	if [ "$1" == "run" ] || [ "$2" == "run" ]; then
 		java -cp .:sqlite-jdbc-3.18.0.jar View
 		#java View
 	fi
