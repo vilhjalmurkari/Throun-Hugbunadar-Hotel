6775d36d03fa97792815ecff7e752f94095b999f
k <k@htgi-dw0857.rhi.hi.is>
2018-04-18 17:19:32 +0000

    Comment fyrir öll föll.

--- src/hotelAPI/AdminAPI.java
+++ src/hotelAPI/AdminAPI.java
@@ -32,54 +32,54 @@ class AdminAPI {
 		DBmanager.setRoomPrice(new_price, rooms);
 	}
 
-	// Notkun: changeRoomPriceByAmount
-	// Fyrir:  
-	// Eftir:  
+	// Notkun: changeRoomPriceByAmount(p,r)
+	// Fyrir:  p er heiltöluverðbreyting, r er herbergi.
+	// Eftir:  Verði r hefur hækkað um p í gagnagrunni og hlut.
 	public void changeRoomPriceByAmount(int price_change, Room room) throws SQLException {
 		DBmanager.changeRoomPriceByAmount(price_change, room);
 		room.price += price_change;
 	}
 
-	// Notkun: 
-	// Fyrir:  
-	// Eftir:  
+	// Notkun: changeRoomPriceByPercent(p,r)
+	// Fyrir:  p er hækkun í broti (t.d. 0,13 fyrir 13% hækkun), r er herbergi.
+	// Eftir:  Verði r hefur hækkað um p í gagnagrunni og hlut.
 	public void changeRoomPriceByPercent(double percent, Room room) throws SQLException {
 		DBmanager.changeRoomPriceByPercent(percent, room);
 		room.price = (int)((1+percent) * room.price);
 	}
 
-	// Notkun: 
-	// Fyrir:  
-	// Eftir:  
+	// Notkun: addHotel(h)
+	// Fyrir:  h er hótel.
+	// Eftir:  h hefur verið bætt við gagnagrunn.
 	public void addHotel(Hotel hotel) throws SQLException {
 		DBmanager.addHotel(hotel);
 	}
 
-	// Notkun: 
-	// Fyrir:  
-	// Eftir:  
+	// Notkun: addHotels(h)
+	// Fyrir:  h er listi hótela.
+	// Eftir:  Öllum hótelum í h hefur verið bætt við í gagnagrunn.
 	public void addHotels(ArrayList<Hotel> hotels) throws SQLException {
 		DBmanager.addHotels(hotels);
 	}
 
-	// Notkun: 
-	// Fyrir:  
-	// Eftir:  
+	// Notkun: addRoomToHotel(r,h)
+	// Fyrir:  r er herbergi, h er hótel.
+	// Eftir:  r hefur verið bætt við í h í gagnagrunni og hlut.
 	public void addRoomToHotel(Room room, Hotel hotel) throws SQLException {
 		hotel.rooms.add(room);
 		DBmanager.addRoomToHotel(room, hotel.name, hotel.city);
 	}
 
-	// Notkun: 
-	// Fyrir:  
-	// Eftir:  
+	// Notkun: deleteHotel(h)
+	// Fyrir:  h er hótel.
+	// Eftir:  h hefur verið eytt úr gagnagrunni.
 	public void deleteHotel(Hotel hotel) throws SQLException {
 		DBmanager.deleteHotel(hotel);
 	}
 
-	// Notkun: 
-	// Fyrir:  
-	// Eftir:  
+	// Notkun: deleteRoom(r)
+	// Fyrir:  r er herbergi.
+	// Eftir:  r hefur verið eytt úr gagnagrunni.
 	public void deleteRoom(Room room) throws SQLException {
 		DBmanager.deleteRoom(room);
 	}
